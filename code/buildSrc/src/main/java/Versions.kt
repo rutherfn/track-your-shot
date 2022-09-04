3d0e8c406fa2ce9ae9817a7df5aa69ce9f445c75
Nicholas Rutherford <nrutherf@BRK-204081.attlocal.net>
2022-09-03 19:42:10 -0500

    add rest of versions consts

--- code/buildSrc/src/main/java/Versions.kt
+++ code/buildSrc/src/main/java/Versions.kt
@@ -1,4 +1,6 @@
 object Versions {
-    const val espressoCore = "3.4.0"
+    const val appCompat = "1.4.1"
     const val junit = "4.13.2"
+    const val ktx = "1.7.0"
+    const val material = "1.5.0"
 }
