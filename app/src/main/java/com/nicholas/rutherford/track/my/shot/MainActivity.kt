54a8d949b9575c6111359f44633686f7d3a873a8
Nicholas Rutherford <nrutherf@BRK-204081.attlocal.net>
2022-09-04 11:39:19 -0500

    error out

--- app/src/main/java/com/nicholas/rutherford/track/my/shot/MainActivity.kt
+++ app/src/main/java/com/nicholas/rutherford/track/my/shot/MainActivity.kt
@@ -9,7 +9,7 @@ class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
+        setContent {dadadadad
             Text(text = "Hello World")
         }
     }
