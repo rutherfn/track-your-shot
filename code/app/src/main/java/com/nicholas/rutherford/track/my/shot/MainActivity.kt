1f3fe76d18d6699efab4d480af0abb213d3a8c19
Nicholas Rutherford <nrutherf@BRK-204081.attlocal.net>
2022-09-04 03:09:41 -0500

    Try again

--- code/app/src/main/java/com/nicholas/rutherford/track/my/shot/MainActivity.kt
+++ code/app/src/main/java/com/nicholas/rutherford/track/my/shot/MainActivity.kt
@@ -10,7 +10,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Text(text = "Hellodddsds Wdadadorld")
+            Text(text = "Helloddsdsddsds Wdadadorld")
         }
     }
 }
