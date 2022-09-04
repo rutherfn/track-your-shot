d2e160d3cb88b7fe83c3ce0a5e3b79cc104cf146
Nicholas Rutherford <nrutherf@BRK-204081.attlocal.net>
2022-09-03 23:20:34 -0500

    oops clean up

--- code/app/src/test/java/com/nicholas/rutherford/track/my/shot/ExampleUnitTest.kt
+++ code/app/src/test/java/com/nicholas/rutherford/track/my/shot/ExampleUnitTest.kt
@@ -1,19 +1,12 @@
 package com.nicholas.rutherford.track.my.shot
 
 import org.junit.jupiter.api.Assertions.assertEquals
-import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 
-/**
- * Example local unit test, which will execute on the development machine (host).
- *
- * See [testing documentation](http://d.android.com/tools/testing).
- */
 class ExampleUnitTest {
 
     @Test
-    @DisplayName("Test with Custom Display Name in nested class")
-    internal fun testInNestedClass() {
-        assertEquals("LOL", "LO" + "L")
+    internal fun testUnitTest() {
+        assertEquals(2 + 2, 4)
     }
 }
--- code/buildSrc/src/main/kotlin/Versions.kt
+++ code/buildSrc/src/main/kotlin/Versions.kt
@@ -7,10 +7,6 @@ object Versions {
         const val kotlin = "1.6.10"
     }
 
-//    testImplementation("org.junit.jupiter:junit-jupiter-api:5.7.2")
-//    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.7.2")
-//    testImplementation("org.junit.jupiter:junit-jupiter-params:5.7.2")
-
     /**
      *  To define dependencies versions */
     object Dependencies {
