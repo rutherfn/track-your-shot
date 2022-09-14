f58a94c7a244c970e032a3b084b75e67a7b7f89d
Nicholas Rutherford <nrutherf@BRK-204081.attlocal.net>
2022-09-13 22:12:41 -0500

    generate correct build variants

--- buildSrc/src/main/kotlin/types/Build.kt
+++ buildSrc/src/main/kotlin/types/Build.kt
@@ -3,6 +3,9 @@ package types
 /**
  * To define build type data */
 interface Build {
+    var appName: String
+    var applicationIdSuffix: String
     var buildName: String
+    var isDebuggable: Boolean
     var isMinifyEnabled: Boolean
 }
