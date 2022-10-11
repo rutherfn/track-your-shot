bee2cdcdb8ae35ca318ccaae87b61430c56f25f1
Nicholas Rutherford <nrutherf@BRK-204081.attlocal.net>
2022-10-10 23:34:27 -0500

    remove firebase

--- buildSrc/src/main/java/Firebase.kt
+++
@@ -1,7 +0,0 @@
-
-/**
- * To define Firebase build data */
-object Firebase {
-    private const val bomVersionNumber = "30.5.0"
-    const val bom = "com.google.firebase:firebase-bom:$bomVersionNumber"
-}
