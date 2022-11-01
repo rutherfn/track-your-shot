688c260b4042a6bf98cb37c6a248da8e6c2fa74e
Nicholas Rutherford <nrutherf@BRK-204081.attlocal.net>
2022-11-01 17:32:50 -0500

    files are getting corrputed build.gradle.kts updates

--- base-resources/build.gradle.kts
+++ base-resources/build.gradle.kts
@@ -3,11 +3,56 @@ plugins {
 }
 
 android {
-    defaultConfig {
-        minSdk = ConfigurationData.minSdk
+    buildToolsVersion = ConfigurationData.buildToolsVersion
+    compileSdk = ConfigurationData.compileSdk
+
+    compileOptions {
+        sourceCompatibility = types.BuildTypes.CompileOptions.sourceCompatibility
+        targetCompatibility = types.BuildTypes.CompileOptions.targetCompatibility
     }
 
-    compileSdk = ConfigurationData.compileSdk
+    defaultConfig {
+        minSdk = ConfigurationData.minSdk
+        targetSdk = ConfigurationData.targetSdk
+
+        testInstrumentationRunner = ConfigurationData.testInstrumentationRunner
+    }
+
+    buildTypes {
+        getByName(types.BuildTypes.UniqueBuilds.Release.buildName) {
+            isMinifyEnabled = types.BuildTypes.UniqueBuilds.Release.isMinifyEnabled
+            proguardFiles(
+                getDefaultProguardFile(types.BuildTypes.proguardAndroidOptimizeTxt),
+                types.BuildTypes.proguardRulesPro
+            )
+        }
+
+        getByName(types.BuildTypes.UniqueBuilds.Debug.buildName) {
+            isMinifyEnabled = types.BuildTypes.UniqueBuilds.Debug.isMinifyEnabled
+        }
+
+        create(types.BuildTypes.UniqueBuilds.Stage.buildName) {
+            initWith(getByName(types.BuildTypes.UniqueBuilds.Debug.buildName))
+            isMinifyEnabled = types.BuildTypes.UniqueBuilds.Stage.isMinifyEnabled
+        }
+    }
+    sourceSets {
+        getByName("main") {
+            java {
+                srcDirs("src/main/java", "src/main/test")
+            }
+        }
+    }
+
+    tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+        kotlinOptions {
+            jvmTarget = KotlinOptions.jvmTarget
+        }
+    }
+
+    tasks.withType<Test> {
+        useJUnitPlatform()
+    }
 }
 
 dependencies {}
