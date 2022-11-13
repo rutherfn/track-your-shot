8680361f912a471b2b05961ede43b535fdda808f
Nicholas Rutherford <nrutherf@BRK-204081.attlocal.net>
2022-11-13 16:07:34 -0600

    add base logic of login

--- navigation/src/main/java/com/nicholas/rutherford/track/my/shot/navigation/NavigationDestinations.kt
+++ navigation/src/main/java/com/nicholas/rutherford/track/my/shot/navigation/NavigationDestinations.kt
@@ -1,6 +1,7 @@
 package com.nicholas.rutherford.track.my.shot.navigation
 
 object NavigationDestinations {
-    const val SPLASH_SCREEN = "splashScreen"
     const val HOME_SCREEN = "homeScreen"
+    const val LOGIN_SCREEN = "loginScreen"
+    const val SPLASH_SCREEN = "splashScreen"
 }
--- navigation/src/test/java/com/nicholas/rutherford/track/my/shot/navigation/NavigationActionsTest.kt
+++ navigation/src/test/java/com/nicholas/rutherford/track/my/shot/navigation/NavigationActionsTest.kt
@@ -8,9 +8,11 @@ import com.nicholas.rutherford.track.my.shot.navigation.NavigationActions as Act
 
 class NavigationActionsTest {
 
-    @Nested inner class NavigationActions {
+    @Nested
+    inner class NavigationActions {
 
-        @Nested inner class SplashScreen {
+        @Nested
+        inner class SplashScreen {
 
             @Test
             fun home() {
@@ -26,16 +28,49 @@ class NavigationActionsTest {
                         .build()
                 )
             }
-        }
-
-        @Nested inner class Home {
 
             @Test
-            fun splash() {
+            fun login() {
                 Assertions.assertEquals(
-                    Actions.HomeScreen.splash().destination,
-                    NavigationDestinations.SPLASH_SCREEN
+                    Actions.SplashScreen.login().destination,
+                    NavigationDestinations.LOGIN_SCREEN
                 )
+                Assertions.assertEquals(
+                    Actions.SplashScreen.login().navOptions,
+                    NavOptions.Builder()
+                        .setPopUpTo(0, true)
+                        .setLaunchSingleTop(true)
+                        .build()
+                )
+            }
+
+            @Nested inner class Login {
+
+                @Test fun home() {
+                    Assertions.assertEquals(
+                        Actions.LoginScreen.home().destination,
+                        NavigationDestinations.HOME_SCREEN
+                    )
+                    Assertions.assertEquals(
+                        Actions.LoginScreen.home().navOptions,
+                        NavOptions.Builder()
+                            .setPopUpTo(0, true)
+                            .setLaunchSingleTop(true)
+                            .build()
+                    )
+                }
+            }
+
+            @Nested
+            inner class Home {
+
+                @Test
+                fun splash() {
+                    Assertions.assertEquals(
+                        Actions.HomeScreen.splash().destination,
+                        NavigationDestinations.SPLASH_SCREEN
+                    )
+                }
             }
         }
     }
