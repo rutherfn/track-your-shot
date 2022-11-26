cb72f8b48f13a04a521afba2fe4425dfa490bddd
Nicholas Rutherford <nrutherf@BRK-204081.attlocal.net>
2022-11-25 18:21:06 -0600

    clean up wild card imports

--- feature/login/src/main/java/com/nicholas/rutherford/track/my/shot/feature/login/LoginScreen.kt
+++ feature/login/src/main/java/com/nicholas/rutherford/track/my/shot/feature/login/LoginScreen.kt
@@ -1,11 +1,6 @@
 package com.nicholas.rutherford.track.my.shot.feature.login
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.ClickableText
-import androidx.compose.material.Button
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
@@ -13,6 +8,11 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.unit.dp
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.Box
 
 @Composable
 fun LoginScreen(viewModel: LoginViewModel) {
@@ -27,5 +27,11 @@ fun LoginScreen(viewModel: LoginViewModel) {
             onClick = { }
         )
     }
-    Button(onClick = { viewModel.onButtonClickedTest() }, content = { Text(text = "Login Button") })
+
+    Column(
+        modifier = Modifier.padding(20.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+    }
 }
