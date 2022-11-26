f6b131c658dbe75bcec8996f0bff603930698d5e
Nicholas Rutherford <nrutherf@BRK-204081.attlocal.net>
2022-11-25 22:56:18 -0600

    add google

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -1,17 +1,10 @@
 import org.gradle.kotlin.dsl.`kotlin-dsl`
 
 plugins {
-    kotlin("jvm") version "1.7.0"
     `kotlin-dsl`
 }
 
 repositories {
     mavenCentral()
-}
-
-dependencies {
-
-    compileOnly(gradleApi())
-
-    implementation(kotlin("stdlib"))
+    google()
 }
