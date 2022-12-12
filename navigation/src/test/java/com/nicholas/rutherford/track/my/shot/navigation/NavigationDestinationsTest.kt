2c45bfec8f8f808245f72d01232f7cded10a3e67
Nicholas Rutherford <nrutherf@BRK-204081.local>
2022-12-12 10:54:24 -0600

    fix and add unit tests

--- navigation/src/test/java/com/nicholas/rutherford/track/my/shot/navigation/NavigationDestinationsTest.kt
+++ navigation/src/test/java/com/nicholas/rutherford/track/my/shot/navigation/NavigationDestinationsTest.kt
@@ -9,6 +9,7 @@ class NavigationDestinationsTest {
 
     lateinit var navigationDestinations: NavigationDestinations
 
+    internal val forgotPasswordScreen = "forgotPasswordScreen"
     internal val homeScreen = "homeScreen"
     internal val loginScreen = "loginScreen"
     internal val splashScreen = "splashScreen"
@@ -20,6 +21,10 @@ class NavigationDestinationsTest {
 
     @Nested
     inner class Constants {
+
+        @Test fun `forgot password screen name should result in forgot screen`() {
+            Assertions.assertEquals(navigationDestinations.FORGOT_PASSWORD_SCREEN, forgotPasswordScreen)
+        }
 
         @Test
         fun `home screen name should result in home screen`() {
