9d124d35890d2101ad6854b47354ea0da36b2447
Nicholas Rutherford <nrutherf@BRK-204081.local>
2022-12-16 15:00:30 -0600

    add comments for component

--- compose-components/src/main/java/com/nicholas/rutherford/track/my/shot/compose/components/ContentWithTopBackAppBar.kt
+++ compose-components/src/main/java/com/nicholas/rutherford/track/my/shot/compose/components/ContentWithTopBackAppBar.kt
@@ -1,13 +1,11 @@
 package com.nicholas.rutherford.track.my.shot.compose.components
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
-import androidx.compose.material.Icon
-import androidx.compose.material.IconButton
-import androidx.compose.material.Text
-import androidx.compose.material.TopAppBar
+import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.Composable
@@ -16,6 +14,14 @@ import androidx.compose.ui.res.stringResource
 import com.nicholas.rutherford.track.my.shot.feature.splash.StringsIds
 import com.nicholas.rutherford.track.my.shot.helper.ui.Padding
 
+/**
+ * Default Content with Top back arrow bar, used for content views
+ *
+ * @param toolbarTitle sets the title of the [TopAppBar]
+ * @param onBackButtonClicked executes whenever the user clicks the [TopAppBar] back button
+ * @param content [Composable] used to set body of content below the [TopAppBar]
+ * @param iconContentDescription optional param responsible for setting description for back arrow button
+ */
 @Composable
 fun ContentWithTopBackAppBar(
     toolbarTitle: String,
