c0ef9e6bee15438244ad7847f7e65445df5a528a
Nicholas Rutherford <nrutherf@BRK-204081.local>
2022-12-17 11:40:22 -0600

    abstract to a new component

--- feature/create-account/src/main/java/com/nicholas/rutherford/track/my/shot/feature/create/account/CreateAccountScreen.kt
+++ feature/create-account/src/main/java/com/nicholas/rutherford/track/my/shot/feature/create/account/CreateAccountScreen.kt
@@ -1,14 +1,11 @@
 package com.nicholas.rutherford.track.my.shot.feature.create.account
 
-import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.requiredWidth
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
@@ -16,18 +13,15 @@ import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Button
 import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.Text
-import androidx.compose.material.TextField
-import androidx.compose.material.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clipToBounds
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.KeyboardType
-import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.unit.dp
 import com.nicholas.rutherford.track.my.shot.compose.components.ContentWithTopBackAppBar
+import com.nicholas.rutherford.track.my.shot.compose.components.TextFieldNoPadding
 import com.nicholas.rutherford.track.my.shot.feature.splash.Colors
 import com.nicholas.rutherford.track.my.shot.feature.splash.StringsIds
 import com.nicholas.rutherford.track.my.shot.helper.ui.Padding
@@ -65,73 +59,35 @@ fun CreateAccountScreenContent(
 
         Spacer(modifier = Modifier.height(Padding.four))
 
-        BoxWithConstraints(
-            modifier = Modifier.clipToBounds()
-        ) {
-            TextField(
-                label = { Text(text = stringResource(id = StringsIds.userName)) },
-                modifier = Modifier
-                    .requiredWidth(maxWidth + Padding.sixteen)
-                    .offset(x = (-8).dp)
-                    .fillMaxWidth(),
-                value = state.username ?: stringResource(id = StringsIds.empty),
-                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Text),
-                onValueChange = {
-                    newUsername ->
-                    viewModel.onUsernameValueChanged(newUsername = newUsername)
-                },
-                textStyle = TextStyles.body,
-                singleLine = true,
-                colors = TextFieldDefaults.textFieldColors(backgroundColor = Colors.whiteColor)
-            )
-        }
+        TextFieldNoPadding(
+            label = stringResource(id = StringsIds.userName),
+            value = state.username ?: stringResource(id = StringsIds.empty),
+            onValueChange = { newUsername ->
+                viewModel.onUsernameValueChanged(newUsername = newUsername)
+            }
+        )
 
         Spacer(modifier = Modifier.height(Padding.four))
 
-        BoxWithConstraints(
-            modifier = Modifier.clipToBounds()
-        ) {
-            TextField(
-                label = { Text(text = stringResource(id = StringsIds.email)) },
-                modifier = Modifier
-                    .requiredWidth(maxWidth + Padding.sixteen)
-                    .offset(x = (-8).dp)
-                    .fillMaxWidth(),
-                value = state.email ?: stringResource(id = StringsIds.empty),
-                onValueChange = {
-                    newEmail ->
-                    viewModel.onEmailValueChanged(newEmail = newEmail)
-                },
-                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Email),
-                textStyle = TextStyles.body,
-                singleLine = true,
-                colors = TextFieldDefaults.textFieldColors(backgroundColor = Colors.whiteColor)
-            )
-        }
+        TextFieldNoPadding(
+            label = stringResource(id = StringsIds.email),
+            value = state.email ?: stringResource(id = StringsIds.empty),
+            onValueChange = { newEmail ->
+                viewModel.onEmailValueChanged(newEmail = newEmail)
+            },
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Email)
+        )
 
         Spacer(modifier = Modifier.height(Padding.four))
 
-        BoxWithConstraints(
-            modifier = Modifier.clipToBounds()
-        ) {
-            TextField(
-                label = { Text(text = stringResource(id = StringsIds.password)) },
-                modifier = Modifier
-                    .requiredWidth(maxWidth + Padding.sixteen)
-                    .offset(x = (-8).dp)
-                    .fillMaxWidth(),
-                value = state.password ?: stringResource(id = StringsIds.empty),
-                onValueChange = {
-                    newPassword ->
-                    viewModel.onPasswordValueChanged(newPassword = newPassword)
-                },
-                visualTransformation = PasswordVisualTransformation(),
-                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
-                textStyle = TextStyles.body,
-                singleLine = true,
-                colors = TextFieldDefaults.textFieldColors(backgroundColor = Colors.whiteColor)
-            )
-        }
+        TextFieldNoPadding(
+            label = stringResource(id = StringsIds.password),
+            value = state.password ?: stringResource(id = StringsIds.empty),
+            onValueChange = { newPassword ->
+                viewModel.onPasswordValueChanged(newPassword = newPassword)
+            },
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password)
+        )
 
         Spacer(modifier = Modifier.height(Padding.eight))
 
