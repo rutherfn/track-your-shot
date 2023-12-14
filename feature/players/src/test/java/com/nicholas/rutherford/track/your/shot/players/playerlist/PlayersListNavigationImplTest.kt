e19116bc3d430c8cd6aa5a412a838f18bf4ce3b5
Nicholas Rutherford <nicholasrutherford@Nicholass-MBP.lan>
2023-12-14 13:48:20 -0600

    navigation impl tests

--- feature/players/src/test/java/com/nicholas/rutherford/track/your/shot/players/playerlist/PlayersListNavigationImplTest.kt
+++ feature/players/src/test/java/com/nicholas/rutherford/track/your/shot/players/playerlist/PlayersListNavigationImplTest.kt
@@ -1,3 +1,72 @@
 package com.nicholas.rutherford.track.your.shot.players.playerlist
 
-class PlayersListNavigationImplTest
+import com.nicholas.rutherford.track.your.shot.data.shared.alert.Alert
+import com.nicholas.rutherford.track.your.shot.data.shared.progress.Progress
+import com.nicholas.rutherford.track.your.shot.feature.players.playerlist.PlayersListNavigationImpl
+import com.nicholas.rutherford.track.your.shot.navigation.NavigationAction
+import com.nicholas.rutherford.track.your.shot.navigation.NavigationActions
+import com.nicholas.rutherford.track.your.shot.navigation.Navigator
+import io.mockk.CapturingSlot
+import io.mockk.mockk
+import io.mockk.slot
+import io.mockk.verify
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+
+class PlayersListNavigationImplTest {
+
+    private lateinit var playersListNavigationImpl: PlayersListNavigationImpl
+
+    private var navigator = mockk<Navigator>(relaxed = true)
+
+    @BeforeEach
+    fun beforeEach() {
+        playersListNavigationImpl = PlayersListNavigationImpl(navigator = navigator)
+    }
+
+    @Test
+    fun `alert action`() {
+        val alert = Alert(title = "title")
+
+        playersListNavigationImpl.alert(alert = alert)
+
+        verify { navigator.alert(alertAction = alert) }
+    }
+
+    @Test
+    fun `disable progress`() {
+        playersListNavigationImpl.disableProgress()
+
+        verify { navigator.progress(progressAction = null) }
+    }
+
+    @Test
+    fun `enable progress`() {
+        val progress = Progress()
+        playersListNavigationImpl.enableProgress(progress = progress)
+
+        verify { navigator.progress(progressAction = progress) }
+    }
+
+    @Test
+    fun `navigate to create player`() {
+        val argumentCapture: CapturingSlot<NavigationAction> = slot()
+
+        playersListNavigationImpl.navigateToCreatePlayer()
+
+        verify { navigator.navigate(capture(argumentCapture)) }
+
+        val capturedArgument = argumentCapture.captured
+        val expectedAction = NavigationActions.PlayersList.createPlayer()
+
+        Assertions.assertEquals(expectedAction.destination, capturedArgument.destination)
+    }
+
+    @Test
+    fun `open navigation drawer`() {
+        playersListNavigationImpl.openNavigationDrawer()
+
+        verify { navigator.showNavigationDrawer(navigationDrawerAction = true) }
+    }
+}
