0aa3d77ba9dcfbd22104fb28c5edbd28fdb31200
Nicholas Rutherford <nicholasrutherford@Nicholass-MBP.lan>
2023-12-20 00:27:08 -0600

    fix one set of test

--- feature/players/src/test/java/com/nicholas/rutherford/track/your/shot/players/PlayersAdditionUpdatesImplTest.kt
+++ feature/players/src/test/java/com/nicholas/rutherford/track/your/shot/players/PlayersAdditionUpdatesImplTest.kt
@@ -1,14 +1,8 @@
 package com.nicholas.rutherford.track.your.shot.players
 
-import com.nicholas.rutherford.track.your.shot.data.test.room.TestPlayer
 import com.nicholas.rutherford.track.your.shot.feature.players.PlayersAdditionUpdatesImpl
-import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.flow.take
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.test.runBlockingTest
 import kotlinx.coroutines.test.runTest
 import org.junit.jupiter.api.Assertions
-import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
@@ -22,52 +16,25 @@ class PlayersAdditionUpdatesImplTest {
         playersAdditionUpdatesImpl = PlayersAdditionUpdatesImpl()
     }
 
-    @Test
-    fun `newPlayerHasBeenAddedSharedFlow emits correct values`() = runTest {
-        // Given
-        val playerUpdates = PlayersAdditionUpdatesImpl()
-        val expectedValues = listOf(true, false, true, true, false)
+    @Nested
+    inner class NewPlayerHasBeenAddedSharedFlow {
+        @Test
+        fun `should initially be set to null if not updated`() = runTest {
+            val result = playersAdditionUpdatesImpl.newPlayerHasBeenAddedMutableSharedFlow.replayCache.firstOrNull()
 
-        // When
-        val actualValues = mutableListOf<Boolean>()
-        val job = launch {
-            playerUpdates.newPlayerHasBeenAddedSharedFlow
-                .take(expectedValues.size)
-                .collect { actualValues.add(it) }
+            Assertions.assertEquals(null, result)
         }
 
-        expectedValues.forEach {
-            playerUpdates.updateNewPlayerHasBeenAddedSharedFlow(it)
+        @Test
+        fun `should be updated if updateNewPlayerAddedFlow is called`() = runTest {
+            val playersAdditionUpdatesImpl = PlayersAdditionUpdatesImpl()
+            val expectedValue = true
+
+            playersAdditionUpdatesImpl.updateNewPlayerHasBeenAddedSharedFlow(expectedValue)
+
+            val result = playersAdditionUpdatesImpl.newPlayerHasBeenAddedMutableSharedFlow.replayCache.firstOrNull()
+
+            Assertions.assertEquals(expectedValue, result)
         }
-
-        // Then
-        assertEquals(expectedValues, actualValues)
-
-        // Cleanup
-        job.cancel()
-    }
-
-    @Test
-    fun `newPlayerHasBeenAddedSharedFlow replays values`() = runTest {
-        // Given
-        val playerUpdates = PlayersAdditionUpdatesImpl()
-        val expectedValues = listOf(true, false, true, true, false)
-
-        // When
-        val actualValues = mutableListOf<Boolean>()
-        expectedValues.forEach {
-            playerUpdates.updateNewPlayerHasBeenAddedSharedFlow(it)
-        }
-
-        val job = launch {
-            playerUpdates.newPlayerHasBeenAddedSharedFlow
-                .collect { actualValues.add(it) }
-        }
-
-        // Then
-        assertEquals(expectedValues, actualValues)
-
-        // Cleanup
-        job.cancel()
     }
 }
