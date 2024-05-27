f39cd52af09d15dd7993e701838eaea49083fcd6
Nicholas Rutherford <nicholasrutherford@Nicholass-MBP.lan>
2024-05-27 12:11:14 -0500

    update fake

--- app/src/androidTest/java/com/nicholas/rutherford/track/your/shot/fakes/FakeReadSharedPreferences.kt
+++ app/src/androidTest/java/com/nicholas/rutherford/track/your/shot/fakes/FakeReadSharedPreferences.kt
@@ -3,24 +3,13 @@ package com.nicholas.rutherford.track.your.shot.fakes
 import com.nicholas.rutherford.track.your.shot.shared.preference.read.ReadSharedPreferences
 
 class FakeReadSharedPreferences(
-    private val accountHasBeenCreated: Boolean = false,
-    private val unverifiedEmail: String? = null,
-    private val unverifiedUsername: String? = null
+    private val appHasBeenLaunched: Boolean = false,
+    private val shouldUpdateLoggedInListState: Boolean = false,
+    private val shouldUpdateLoggedInDeclaredShotListState: Boolean = false
 ) : ReadSharedPreferences {
-    override fun accountHasBeenCreated(): Boolean = accountHasBeenCreated
+    override fun appHasBeenLaunched(): Boolean = appHasBeenLaunched
 
-    override fun unverifiedEmail(): String? = unverifiedEmail
+    override fun shouldUpdateLoggedInPlayerListState(): Boolean = shouldUpdateLoggedInListState
 
-    override fun unverifiedUsername(): String? = unverifiedUsername
-    override fun appHasBeenLaunched(): Boolean {
-        TODO("Not yet implemented")
-    }
-
-    override fun shouldUpdateLoggedInPlayerListState(): Boolean {
-        TODO("Not yet implemented")
-    }
-
-    override fun shouldUpdateLoggedInDeclaredShotListState(): Boolean {
-        TODO("Not yet implemented")
-    }
+    override fun shouldUpdateLoggedInDeclaredShotListState(): Boolean = shouldUpdateLoggedInDeclaredShotListState
 }
