aa0f19ca4c4dcd124bc208c419f0fef84f20e088
Nicholas Rutherford <nicholasrutherford@Nicholass-MBP.lan>
2025-10-01 00:00:41 -0500

    created voice commands module

---
+++ feature/voice-commands/build.gradle.kts
@@ -0,0 +1,85 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
+plugins {
+    id(BuildIds.androidLibrary)
+    kotlin(BuildIds.pluginKotlin)
+    id(BuildIds.ktLintId) version ConfigurationData.ktlintVersion
+    id(BuildIds.kover)
+    alias(libs.plugins.kotlin.compose)
+}
+
+android {
+    namespace = "com.nicholas.rutherford.track.your.shot.feature.voice.commands"
+
+    buildToolsVersion = ConfigurationData.buildToolsVersion
+    compileSdk = ConfigurationData.compileSdk
+
+    buildFeatures {
+        compose = ComposeData.Enabled.value
+    }
+
+    @Suppress("UnstableApiUsage")
+    composeOptions {
+        kotlinCompilerExtensionVersion = ComposeData.KotlinCompiler.extensionVersion
+    }
+
+    compileOptions {
+        sourceCompatibility = types.BuildTypes.CompileOptions.sourceCompatibility
+        targetCompatibility = types.BuildTypes.CompileOptions.targetCompatibility
+    }
+
+    defaultConfig {
+        minSdk = ConfigurationData.minSdk
+
+        testInstrumentationRunner = ConfigurationData.testInstrumentationRunner
+    }
+
+    testOptions {
+        targetSdk = ConfigurationData.targetSdk
+    }
+
+    buildTypes {
+        getByName(types.BuildTypes.UniqueBuilds.Release.buildName) {
+            isMinifyEnabled = types.BuildTypes.UniqueBuilds.Release.isMinifyEnabled
+            proguardFiles(
+                getDefaultProguardFile(types.BuildTypes.proguardAndroidOptimizeTxt),
+                types.BuildTypes.proguardRulesPro
+            )
+        }
+
+        getByName(types.BuildTypes.UniqueBuilds.Debug.buildName) {
+            isMinifyEnabled = types.BuildTypes.UniqueBuilds.Debug.isMinifyEnabled
+        }
+
+        create(types.BuildTypes.UniqueBuilds.Stage.buildName) {
+            initWith(getByName(types.BuildTypes.UniqueBuilds.Debug.buildName))
+            isMinifyEnabled = types.BuildTypes.UniqueBuilds.Stage.isMinifyEnabled
+        }
+    }
+    sourceSets {
+        getByName("main") {
+            java {
+                srcDirs("src/main/java")
+            }
+        }
+    }
+
+    tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+        compilerOptions {
+            jvmTarget.set(JvmTarget.fromTarget(KotlinOptions.jvmTarget))
+        }
+    }
+
+    tasks.withType<Test> {
+        useJUnitPlatform()
+    }
+}
+
+dependencies {
+    debugImplementation(libs.androidx.ui.test.manifest)
+
+    implementation(libs.androidx.material3)
+    implementation(libs.androidx.lifecycle.viewmodel.compose)
+
+    implementation(libs.androidx.ui.tooling.preview)
+}
