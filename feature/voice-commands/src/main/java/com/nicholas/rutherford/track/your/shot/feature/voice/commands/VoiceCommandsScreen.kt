df2bf17b7c95e4323aa8d5abf6c847e445513dc6
Nicholas Rutherford <nicholasrutherford@Nicholass-MBP.lan>
2025-10-05 20:52:12 -0500

    added compose showkase

--- feature/voice-commands/src/main/java/com/nicholas/rutherford/track/your/shot/feature/voice/commands/VoiceCommandsScreen.kt
+++ feature/voice-commands/src/main/java/com/nicholas/rutherford/track/your/shot/feature/voice/commands/VoiceCommandsScreen.kt
@@ -9,24 +9,17 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Done
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.FilterChip
 import androidx.compose.material3.FilterChipDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import com.nicholas.rutherford.track.your.shot.data.room.response.SavedVoiceCommand
-import com.nicholas.rutherford.track.your.shot.data.room.response.VoiceCommandTypes.Companion.toDisplayLabel
 
 @Composable
 fun VoiceCommandsScreen(params: VoiceCommandsParams) {
