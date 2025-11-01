2102b4462ebc8356c80fa66be6ada0cfe20c6df7
Nicholas Rutherford <nicholasrutherford@Nicholass-MBP.lan>
2025-11-01 08:11:59 -0500

    fix import issues

--- compose/components/src/main/java/com/nicholas/rutherford/track/your/shot/compose/components/RecordingIndicator.kt
+++ compose/components/src/main/java/com/nicholas/rutherford/track/your/shot/compose/components/RecordingIndicator.kt
@@ -14,6 +14,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.MaterialTheme
@@ -28,7 +29,6 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.foundation.layout.padding
 import com.nicholas.rutherford.track.your.shot.AppColors
 import com.nicholas.rutherford.track.your.shot.base.resources.Colors
 
